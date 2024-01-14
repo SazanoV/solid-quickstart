375fbbbc4b262ca77d8fd28f2380de67f5fe6995
SazanoV <72541034+SazanoV@users.noreply.github.com>
2024-01-14 08:22:10 +0400

    Update root.tsx
    change links at the main page

--- src/root.tsx
+++ src/root.tsx
@@ -25,8 +25,8 @@ export default function Root() {
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <A href="/">Index</A>
-            <A href="/about">About</A>
+            <A href="/">Main page</A>
+            <A href="/about">About me</A>
             <Routes>
               <FileRoutes />
             </Routes>
